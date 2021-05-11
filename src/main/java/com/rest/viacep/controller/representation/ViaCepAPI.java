d750c81b0809cc425ecbf10604f43ab9bf3eda57
Felipe Carvalho <felipee_sc@hotmail.com>
2021-05-10 21:52:55 -0300

    implementing exception

--- src/main/java/com/rest/viacep/controller/representation/ViaCepAPI.java
+++ src/main/java/com/rest/viacep/controller/representation/ViaCepAPI.java
@@ -1,12 +1,14 @@
 package com.rest.viacep.controller.representation;
 
 import com.rest.viacep.controller.representation.response.EnderecoResponse;
+import com.rest.viacep.domain.exception.ViaCepNotFoundException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 @Api(value = "Api responsavel por manter consultas ao VIA CEP", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -19,5 +21,6 @@ public interface ViaCepAPI {
                     response = EnderecoResponse.class),
             @ApiResponse(code = 403, message = " Não autorizado!")
     })
+    @ExceptionHandler(ViaCepNotFoundException.class)
     ResponseEntity<EnderecoResponse> obtemCep(String cep);
 }
--- src/main/java/com/rest/viacep/services/ViaCepServiceImpl.java
+++ src/main/java/com/rest/viacep/services/ViaCepServiceImpl.java
@@ -2,6 +2,7 @@ package com.rest.viacep.services;
 
 import com.rest.viacep.config.RestConfiguration;
 import com.rest.viacep.domain.dto.EnderecoDTO;
+import com.rest.viacep.domain.exception.ViaCepNotFoundException;
 import com.rest.viacep.services.service.ViaCepService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +21,22 @@ public class ViaCepServiceImpl implements ViaCepService {
 
     @Override
     public EnderecoDTO obtemCep(String cep) {
+        Map<String, String> params = getStringStringMap(cep);
+        EnderecoDTO enderecoDTO = callCep(params);
+        return enderecoDTO;
+    }
+
+    private EnderecoDTO callCep(Map<String, String> params) {
+        EnderecoDTO enderecoDTO = rest.restTemplate().getForObject(uri, EnderecoDTO.class, params);
+        if (enderecoDTO.getUf() == null){
+            throw new ViaCepNotFoundException("Cep não encontrado!");
+        }
+        return enderecoDTO;
+    }
+
+    private Map<String, String> getStringStringMap(String cep) {
         Map<String, String> params = new HashMap<String, String>();
         params.put("cep", cep);
-        EnderecoDTO enderecoDTO = rest.restTemplate().getForObject(uri, EnderecoDTO.class, params);
-        return enderecoDTO;
+        return params;
     }
 }
